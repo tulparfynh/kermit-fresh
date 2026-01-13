bb1171365000ffaa165208094a7c6b15fa4dcc0b
Barbaros Ahmet Bayram <barbaros@ardasaglobal.com>
2026-01-13 23:32:12 +0000

    go with step 1

---
+++ src/navigation.ts
@@ -0,0 +1,16 @@
+import {Pathnames} from 'next-intl/navigation';
+
+export const locales = ['en', 'tr'] as const;
+export const defaultLocale = 'en';
+
+export const pathnames = {
+  '/spc-wall-panels': {
+    en: '/spc-wall-panels',
+    tr: '/spc-duvar-panelleri',
+  },
+} satisfies Pathnames<typeof locales>;
+
+// Use the default: `always`
+export const localePrefix = undefined;
+
+export type AppPathnames = keyof typeof pathnames;
